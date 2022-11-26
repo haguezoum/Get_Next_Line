659573cf62a8ca41c5b6b41d26bf81f2f28c3a70
Hassan Aguezoum <haguezou@c1r3p7.1337.ma>
2022-11-26 17:17:25 +0100

    fix leaks and add Makefile

--- get_next_line.c
+++ get_next_line.c
@@ -38,27 +38,24 @@ char *chunck(char *s)
     }
     while(s[i] != '\n' && s[i])
         i++;
-    return (ft_substr((char*)s, i + 1, s_len - (i + 1)));
+    return (ft_substr((char*)s, i + 1, s_len - (i)));
  }
 // end rest of
 // end strjoin
 
-char *st_(int fd, char *line) {
+char *string(int fd, char *line) {
     char *buff;
     int bytes;
-
+    int loop = 0;
     bytes = 1;
     buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
     if(buff == NULL)
         return (NULL);
-    while(bytes != 0)
+    while(bytes > 0)
     {
         bytes = read(fd, buff, BUFFER_SIZE);
-        if(bytes < 0)
-        {
-            free(buff);
-            return(NULL);
-        }
+        if(bytes == -1)
+            break;
         buff[bytes] = '\0';
         line = ft_strjoin(line, buff);
         if (ft_strchr(buff, '\n'))
@@ -76,67 +73,25 @@ char *get_next_line(int fd)
     {
         return (NULL);
     }
-    line = st_(fd, line);
+    line = string(fd, line);
     if (!line)
         return NULL;
     tmp = chunck(line);
     line = restof(line);
     return tmp;
 }
+//main function
+int main()
+{
+    int fd;
+    char *r_str;
+    fd = open("_file.txt" , O_RDONLY);
 
-// int main()
-// {
-//     int fd;
-//     char *str;
-//     str = "hello world \n ggfgf";
-//     char *res = chunck(str);
-//     printf("%s",res);
-//         fd = open("_file.txt",O_RDONLY);
-//    int i = 0;
-//     while(1)
-//     { 
-//         if(str == NULL)
-//             break;
-        
-//          str = get_next_line(fd);
-//          printf("%s", str);
-         
-//         i++;
-//     }
-// }
-
-// int main()
-// {
-//     int fd;
-//     char *str;
-//     fd = open("_file.txt",O_RDONLY);
-
-    
-//     while(1){
-//         str = get_next_line(fd);
-//         printf("%s",str);
-//         break;
-//     }
-// }
-
-
-    //this fucking function dosnt returning NUUUUUUUUL aaas   fgqgriu;gqrt
-  
-    // str = get_next_line(fd);
-    // printf("%s",str);
-    // str = get_next_line(fd);
-    // printf("%s",str);
-    
-    // char *test = "hassan\nand this is a test";
-    // char *mystr;
-    //  mystr = restof(test);
-    //  printf("%s",mystr);
-
-// } main
-
-// int main()
-// {
-//     char *str = "mehdi\0\nhassan\naguezoum";
-//     char *ret = chunck(str);
-//     puts(ret);
-// }
+    while(1)
+    {
+        r_str = get_next_line(fd);
+        printf("%s", r_str);
+        if(r_str == NULL)
+            break;
+    }
+}
